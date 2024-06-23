0fc0f21d34a8fc6d85462b0c0b9bf3fa3ba248c7
Vladimir Balin <vsbalin@mts.ru>
2024-06-23 22:10:15 +0500

    drop the MATCH in favour of the ASSERT with an AssertFunc closure

--- gma.go
+++ gma.go
@@ -2,8 +2,6 @@ package gma
 
 type AssertFunc[T any] func(x T) bool
 
-type MatchFunc[T, P any] func(want P, got T) bool
-
 type Assert[T any] struct {
 	asserter AssertFunc[T]
 	msg      string
@@ -17,43 +15,10 @@ func ASSERT[T any](asserter AssertFunc[T], msg ...string) *Assert[T] {
 	return m
 }
 
-func (m *Assert[T]) Matches(x any) bool {
-	return m.asserter(x.(T))
+func (a *Assert[T]) Matches(x any) bool {
+	return a.asserter(x.(T))
 }
 
-func (m *Assert[T]) String() string {
-	if m.msg != "" {
-		return m.msg
-	}
-
-	return "asserter"
-}
-
-type Match[T, P any] struct {
-	match MatchFunc[T, P]
-	want  P
-	msg   string
-}
-
-func MATCH[T, P any](matcher MatchFunc[T, P], want P, msg ...string) *Match[T, P] {
-	m := &Match[T, P]{
-		match: matcher,
-		want:  want,
-	}
-	if len(msg) > 0 {
-		m.msg = msg[0]
-	}
-	return m
-}
-
-func (m *Match[T, P]) Matches(x any) bool {
-	return m.match(m.want, x.(T))
-}
-
-func (m *Match[T, P]) String() string {
-	if m.msg != "" {
-		return m.msg
-	}
-
-	return "matcher"
+func (a *Assert[T]) String() string {
+	return a.msg
 }
